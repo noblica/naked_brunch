14028bafb71995d5cc1a49613f44169b82f13189
noblica <pljeskavicica@gmail.com>
2016-04-25 10:29:23 +0200

    Initial commit

---
+++ brunch-config.js
@@ -0,0 +1,44 @@
+module.exports = {
+    conventions: {
+        ignored: /.+\.spec\.js/
+    },
+    paths: {
+        public: ''
+    },
+    modules: {
+        definition: false,
+        wrapper: false
+    },
+    files: {
+        javascripts: {
+            joinTo: {
+                'js/app.js': /^app/,
+                'js/libraries.js': /^bower_components/
+            },
+            order: {
+                before: [/^app\/([0-9a-zA-Z]+\/)*[0-9a-zA-Z]+(\.module)?\.js$/]
+            }
+        },
+        stylesheets: {
+            joinTo: {
+                'app.css': /^(app|bower_components)/
+            }
+        },
+        templates: {
+            joinTo: {
+                'js/templates.js': /^app\//
+            }
+        }
+    },
+    plugins: {
+
+        /**
+         * Have to disable it, otherwise browsersync just keeps reloading.
+         */
+        off: [
+            'digest-brunch'],
+        angular_templates: {
+            module: 'app'
+        }
+    }
+};
